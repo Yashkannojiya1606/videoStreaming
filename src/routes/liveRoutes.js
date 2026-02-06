af7359caf67fde34e9a4972bfdd1fcae05e4937f
Yashwant Kannojiya <yashwantkannojiya8@gmail.com>
2026-02-06 17:51:36 +0530

    Fix middleware import name in live routes

--- src/routes/liveRoutes.js
+++ src/routes/liveRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { startLive } from "../controllers/liveController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Go Live (protected route)
-router.post("/start", authMiddleware, startLive);
+router.post("/start", protect, startLive);
 
 export default router;
