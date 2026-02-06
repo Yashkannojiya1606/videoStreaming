b033fb67c78567255d05818c1c5c2c23b9ef4488
Yashwant Kannojiya <yashwantkannojiya8@gmail.com>
2026-02-06 17:02:10 +0530

    Fix authMiddleware import in live routes

--- src/routes/liveRoutes.js
+++ src/routes/liveRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { startLive } from "../controllers/liveController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
